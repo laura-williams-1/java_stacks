9c282826cb595d4ed900217477bb8c857e150f69
laura-williams-1 <laurawilliams@pursuit.org>
2025-10-30 22:56:07 -0400

    inital stack interface

---
+++ src/Stack3130.java
@@ -0,0 +1,22 @@
+public interface Stack3130<E> {
+// interfaces specify:
+    // method name
+    //return type
+    // parameters
+
+    // stack has 4 mehtods - peek, pop, push, ismepty to string
+     E peek();
+
+    boolean isEmpty();
+
+    void push(E e);
+
+    E pop();
+
+    @Override
+    String toString();
+
+
+
+
+}
